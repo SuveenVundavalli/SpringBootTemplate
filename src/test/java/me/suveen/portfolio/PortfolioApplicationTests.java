4fbf2ffbb0c5cf6d9a1159df4ebff3657b2ab1d6
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-03-06 23:26:15 +0530

    Added i18n capabalities to the web skeleton

--- src/test/java/me/suveen/portfolio/PortfolioApplicationTests.java
+++ src/test/java/me/suveen/portfolio/PortfolioApplicationTests.java
@@ -1,7 +1,10 @@
 package me.suveen.portfolio;
 
+import me.suveen.portfolio.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PortfolioApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() {
+		String expectedResult = "Suveen Kumar Vundavalli";
+		String messageId = "index.main.callout";
+		String actualResult = i18NService.getMessage(messageId);
+		Assert.assertEquals("testMessageByLocaleService", expectedResult, actualResult);
 	}
 
 }
